b24c6b17724510afc3e026a2fcc695fa20a86a14
hirokiwa <hirokifsys@gmail.com>
2023-03-10 07:56:58 +0900

    ヘッダー作成

--- src/pages/Content/Content.tsx
+++ src/pages/Content/Content.tsx
@@ -1,10 +1,12 @@
 import { useParams } from "react-router-dom";
+import Header from "../../component/header/Header";
 
 const Content = (): JSX.Element => {
 
     const {contentId} = useParams()
     return (
         <>
+            <Header/>
             <h2>content ID : {contentId}</h2>
         </>
     );
