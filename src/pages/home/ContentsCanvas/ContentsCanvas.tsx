e3103165e3eed227ad44c174d1cd650977177d84
hirokiwa <hirokifsys@gmail.com>
2023-03-10 05:39:33 +0900

    キャンバス設置

---
+++ src/pages/home/ContentsCanvas/ContentsCanvas.tsx
@@ -0,0 +1,113 @@
+import styled from "@emotion/styled";
+import Matter from "matter-js";
+
+const ContentsCanvas = (): JSX.Element => {
+  const Engine = Matter.Engine,
+    Render = Matter.Render,
+    Runner = Matter.Runner,
+    Bodies = Matter.Bodies,
+    Composite = Matter.Composite;
+
+  const engine = Engine.create();
+
+  const render = Render.create({
+    element: document.body,
+    engine: engine,
+    options: {
+      width: window.innerWidth,
+      height: window.innerHeight,
+      background: "FFF",
+      wireframeBackground: "#FFF",
+      wireframes: false,
+      showAngleIndicator: false,
+    },
+  });
+
+  const ground = Bodies.rectangle(
+    window.innerWidth / 2,
+    window.innerHeight,
+    window.innerWidth,
+    60,
+    {
+      isStatic: true,
+      render: {
+        fillStyle: "#24292f",
+      },
+    }
+  );
+
+  const leftWall = Bodies.rectangle(
+    -1,
+    window.innerHeight / 2,
+    2,
+    window.innerHeight,
+    {
+      isStatic: true,
+      friction: 0,
+      render: {
+        fillStyle: "#24292f",
+      },
+    }
+  );
+
+  const rightWall = Bodies.rectangle(
+    window.innerWidth + 1,
+    window.innerHeight / 2,
+    2,
+    window.innerHeight,
+    {
+      isStatic: true,
+      friction: 0,
+      render: {
+        fillStyle: "#24292f",
+      },
+    }
+  );
+
+  let addContentes = [ground, leftWall, rightWall];
+
+  const MAX_QUANTITY = 100;
+  const quantity = Math.floor(Math.random() * MAX_QUANTITY - 1) + 1;
+
+  const BOX_WIDTH = 80;
+  const BOX_HEIGHT = BOX_WIDTH;
+
+  for (let i = 0; i < quantity; i++) {
+    addContentes.push(
+      Bodies.rectangle(
+        Math.random() * (window.innerWidth - BOX_WIDTH - BOX_WIDTH) + BOX_WIDTH,
+        Math.random() * 200,
+        BOX_WIDTH,
+        BOX_HEIGHT
+      )
+    );
+  }
+
+  Composite.add(engine.world, addContentes);
+  Render.run(render);
+  const runner = Runner.create();
+  Runner.run(runner, engine);
+
+  console.log("hello");
+  return (
+    <CanvasArea>
+      <BackGroundMessage>
+        ランダムで1〜{MAX_QUANTITY}個の四角を落としているわよ
+      </BackGroundMessage>
+    </CanvasArea>
+  );
+};
+
+const CanvasArea = styled.div`
+  position: fixed;
+`;
+
+const BackGroundMessage = styled.h2`
+  position: absolute;
+  color: #24292f;
+  white-space: nowrap;
+  top: ${window.innerHeight / 2}px;
+  left: 100px;
+`;
+
+export default ContentsCanvas;
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,21 @@
+import styled from "@emotion/styled";
+import ContentsCanvas from "./ContentsCanvas/ContentsCanvas";
+
 const Home = (): JSX.Element => {
-    return (
-        <>
-            This is Home
-        </>
-    )
-}
+  const disallowScrolling = (event: Event) => {
+    event.preventDefault();
+  };
+
+  document.addEventListener("touchmove", disallowScrolling, { passive: false });
+  document.addEventListener("mousewheel", disallowScrolling, {
+    passive: false,
+  });
+
+  return (
+    <>
+      <ContentsCanvas />
+    </>
+  );
+};
 
 export default Home;
