c821dbc5f69b644e2c0a50bb130db7c6aec24018
hirokiwa <hirokifsys@gmail.com>
2023-03-10 18:51:44 +0900

    マイページ追加

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Content from './pages/Content/Content';
+import MyPage from './pages/MyPage/MyPage';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path={`/`} element={<Home />} />
+        <Route path={`/mypage`} element={<MyPage />} />
         <Route path={`content`}>
           <Route path={`:contentId`} element={<Content/>} />
         </Route>
---
+++ src/pages/MyPage/MyPage.tsx
@@ -0,0 +1,12 @@
+import Header from "../../component/header/Header";
+
+const MyPage = (): JSX.Element => {
+  return (
+    <>
+      <Header></Header>
+      <h2>This is My Page</h2>
+    </>
+  );
+};
+
+export default MyPage;
