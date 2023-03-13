1a4027874baf08fd1121d3dfefe081757a0b6b80
YukaChoco <sugar.ore.yk@gmail.com>
2023-03-14 00:29:22 +0900

    textbox2

--- src/pages/PostContent/PostContent.tsx
+++ src/pages/PostContent/PostContent.tsx
@@ -1,37 +1,82 @@
 import Header from "../../component/header/Header";
 import { useNavigate } from 'react-router-dom';
-import { styled, TextField, withStyles } from "@mui/material";
+import { Button, createMuiTheme, styled, TextField, useFormControl, withStyles } from "@mui/material";
+import { ThemeProvider } from "@emotion/react";
+import react, { useContext, useState } from "react"
 
 const PostContent = (): JSX.Element => {
 
+  const [check, setCheck] = useState(true);
+  const [tittle, setTittle] = useState('');
+  const [comment, setComment] = useState('');
+
+  const theme1 = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#95a5a6"
+      },
+    }
+  });
+  const theme2 = createMuiTheme({
+    palette: {
+      primary: {
+        main: "#7f8c8d"
+      },
+    }
+  });
+
+  const checkFilled = (value: string) => {
+    if (value == '') {
+      setCheck(true);
+    }
+    else {
+      setCheck(false);
+    }
+  }
+
   const navigate = useNavigate();
- //button→投稿ボタンのコンポーネントつくる（全要素そろってるか分岐して違うボタン出す）
+
   return (
     <>
       <Header />
-      <div>
-        {/* <myTextbox>title</myTextbox> */}
-        <TextField id="outlined-basic" label="tittle" variant="outlined" />
-        {/* <input type="text" name="title" placeholder="title"></input> */}
+      {/* <div> */}
+      <div style={{ margin: "2%" }}>
+        <ThemeProvider theme={theme1}>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="tittle" variant="outlined"
+              style={{
+                width: "50%",
+                marginBottom: "2%"
+              }}
+              onChange={(event) => {
+                setTittle(event.target.value);
+                checkFilled(event.target.value);
+              }} />
+          </div>
+          <div>
+            <TextField
+              id="outlined-basic"
+              label="comment"
+              variant="outlined"
+              multiline
+              minRows="10"
+              style={{
+                width: "50%",
+              }}
+              onChange={(event) => {
+                setComment(event.target.value);
+              }}
+            />
+          </div>
+        </ThemeProvider>
       </div>
-      <div>
-        <TextField id="outlined-basic" label="comment" variant="outlined" />
-      </div>
-      <button onClick={() => {navigate("/")}}>post</button>
+      <ThemeProvider theme={theme2}>
+        <Button variant="contained" disabled={check} onClick={() => { navigate(`/content/${tittle}`) }}>post</Button>
+      </ThemeProvider>
     </>
   );
 };
 
 export default PostContent;
-
-// const myTextbox = styled(TextField)`
-//   position: fixed;
-// `;
-
-// const CustomComponent = withStyles({
-//   root: {
-//     // ここにスタイルを書いたり
-//   },
-//   label: {},
-//   // etc...
-// })(TextField)
