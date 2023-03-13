fe3dbd8f3f7173f09246612b5effa9039619e3c3
YukaChoco <sugar.ore.yk@gmail.com>
2023-03-14 03:36:30 +0900

    photoSelectOk

--- src/pages/PostContent/PostContent.tsx
+++ src/pages/PostContent/PostContent.tsx
@@ -3,12 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { Button, createMuiTheme, styled, TextField, useFormControl, withStyles } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
 import react, { useContext, useState } from "react"
+import { grid, height, padding } from "@mui/system";
+import { right } from "@popperjs/core";
 
 const PostContent = (): JSX.Element => {
 
   const [check, setCheck] = useState(true);
   const [tittle, setTittle] = useState('');
   const [comment, setComment] = useState('');
+  const [profileImage, setProfileImage] = useState('default-profile.png');
 
   const theme1 = createMuiTheme({
     palette: {
@@ -36,45 +39,94 @@ const PostContent = (): JSX.Element => {
 
   const navigate = useNavigate();
 
+
+  const SelectPhoto = () => {
+    if(profileImage=='default-profile.png'){
+      return(
+        <input type="file" onChange={onFileInputChange}/>
+      )
+    }
+    else{
+      return(
+        <img
+              src={profileImage}
+              // className="h-32 w-32 rounded-full"
+              style={{
+                width: "31vw",
+                height: "31vw",
+                paddingLeft: "2vw",
+                paddingRight: "2vw",
+              }}
+            />
+      )
+    }
+  }
+
+  const onFileInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files) return;
+
+    // React.ChangeEvent<HTMLInputElement>よりファイルを取得
+    const fileObject = e.target.files[0];
+    // オブジェクトURLを生成し、useState()を更新
+    setProfileImage(window.URL.createObjectURL(fileObject));
+  };
+
   return (
     <>
       <Header />
-      {/* <div> */}
-      <div style={{ margin: "2%" }}>
-        <ThemeProvider theme={theme1}>
-          <div>
-            <TextField
-              id="outlined-basic"
-              label="tittle" variant="outlined"
-              style={{
-                width: "50%",
-                marginBottom: "2%"
-              }}
-              onChange={(event) => {
-                setTittle(event.target.value);
-                checkFilled(event.target.value);
-              }} />
+      <div
+        style={{
+          display: "grid",
+          paddingLeft: "10vw",
+          paddingRight: "10vw",
+          gridTemplateColumns: "35vw 55vw",
+          height: "100vh"
+        }}
+      >
+        <div style={{height: "100%",paddingTop: "20%"}}>
+          <SelectPhoto></SelectPhoto>
+        </div>
+        <div style={{height: "100%",paddingTop: "20%"}}>
+          <div style={{ margin: "2%" }}>
+            <ThemeProvider theme={theme1}>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="tittle" variant="outlined"
+                  style={{
+                    width: "40vw",
+                    marginBottom: "4%"
+                  }}
+                  onChange={(event) => {
+                    setTittle(event.target.value);
+                    checkFilled(event.target.value);
+                  }}
+                />
+              </div>
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="comment"
+                  variant="outlined"
+                  multiline
+                  minRows="7"
+                  style={{
+                    width: "40vw",
+                  }}
+                  onChange={(event) => {
+                    setComment(event.target.value);
+                  }}
+                />
+              </div>
+            </ThemeProvider>
           </div>
-          <div>
-            <TextField
-              id="outlined-basic"
-              label="comment"
-              variant="outlined"
-              multiline
-              minRows="10"
-              style={{
-                width: "50%",
-              }}
-              onChange={(event) => {
-                setComment(event.target.value);
-              }}
-            />
+          <div style={{textAlign: "right", width: "40vw"}}>
+            <ThemeProvider theme={theme2}>
+              <Button variant="contained" size={"large"} disabled={check} style={{flexGrow: "1"}} onClick={() => { navigate(`/content/${tittle}`) }}>post</Button>
+            </ThemeProvider>
           </div>
-        </ThemeProvider>
+        </div>
       </div>
-      <ThemeProvider theme={theme2}>
-        <Button variant="contained" disabled={check} onClick={() => { navigate(`/content/${tittle}`) }}>post</Button>
-      </ThemeProvider>
     </>
   );
 };
