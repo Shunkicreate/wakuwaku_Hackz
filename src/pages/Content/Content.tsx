19ae84ca6b48bccca6125dc503dd71fc10484f8f
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-03-15 12:44:17 +0900

    cors直したら動く

--- src/pages/Content/Content.tsx
+++ src/pages/Content/Content.tsx
@@ -18,36 +18,50 @@ const Content = (): JSX.Element => {
   const [title, setTitle] = useState<string>('');
   const [comment, setComment] = useState<string>('');
   const [heart, setHeart] = useState<boolean>(false);
-
+  const [dateTime, setDateTime] = useState<Date>()
   const [happy, setHappy] = useState<string>("----");
+  const [testUser, setTestUser] = useState<Post | null>()
   const location = useLocation()
-  const myRE =  /index=.*/;
+  const myRE = /index=.*/;
   const path = myRE.exec(location.pathname)
   let index = 0
-  if(path){
+  if (path) {
     index = Number(path[0].replace("index=", ""))
   }
   console.log(location, index)
-
-  let testUser: Post = {
-    post_id: 11,
-    img_url: data(index),
-    // img_url: "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG9dUxBZzDlI4sMQm3qrArYJAVN0KNV4ozNB_n8wwajJQkQ23GJiahCFg1WybqJ_vWY4RSWHm22bmRghKDLettLMsAcPkkKOmJQ5M4O55z8cBSrOIb74hC735APTQwch577nIGXvL5Xb3kBd306ZtJN8IdPg849LO5pfMhRBhNffSKcXX9ctHvjSbSRPnK9YZ26FGAa5hB3xlBz5e7alwoJjijGWsGGmrIz3ODGOMdnS6MbYSBASdUwqgAi6EtoXIYQ==/gahag-0052682376.jpg",
-    title: "パンダさん",
-    description: "パンダさんかわいいなあ",
-    uid: 1,
-    // user: User,
-    alt: "",
-    posted_at: 1567566215, //unix time
-    modified_at: 2, //unix time
-    happiness_rate: 97.1,
-    deleted: true
+  interface sqlDataType {
+    data: { data: Post[] }
   }
+  // let testUser: Post
+  // fetch("https://wakuwaku-backend.azurewebsites.net/get-post").then((res) => {
+  fetch("https://wakuwaku-backend.azurewebsites.net/get-post").then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data);
+      const sqlData = data as sqlDataType
+      const idx = sqlData.data.data.length
+      const showData = sqlData.data.data[Math.floor(Math.random() * idx)]
+      const preTestUser = {
+        post_id: showData.post_id,
+        img_url: data(index),
+        // img_url: "https://msp.c.yimg.jp/images/v2/FUTi93tXq405grZVGgDqG9dUxBZzDlI4sMQm3qrArYJAVN0KNV4ozNB_n8wwajJQkQ23GJiahCFg1WybqJ_vWY4RSWHm22bmRghKDLettLMsAcPkkKOmJQ5M4O55z8cBSrOIb74hC735APTQwch577nIGXvL5Xb3kBd306ZtJN8IdPg849LO5pfMhRBhNffSKcXX9ctHvjSbSRPnK9YZ26FGAa5hB3xlBz5e7alwoJjijGWsGGmrIz3ODGOMdnS6MbYSBASdUwqgAi6EtoXIYQ==/gahag-0052682376.jpg",
+        title: showData.title,
+        description: showData.description,
+        uid: showData.uid,
+        // user: User,
+        alt: "",
+        posted_at: 1567566215, //unix time
+        modified_at: 2, //unix time
+        happiness_rate: showData.happiness_rate,
+        deleted: true
+      }
+      setDateTime(new Date(preTestUser.posted_at * 1000))
+      // let dateTime = new Date(preTestUser.posted_at === null ? 0 : preTestUser.posted_at * 1000);
+      setTestUser(preTestUser)
+    })
 
-  let dateTime = new Date(testUser.posted_at===null?0:testUser.posted_at * 1000);
   // let dateTime = new Date(testUser.posted_at===null?0:Number((testUser.posted_at * 1000).toFixed(-1)));
-  console.log(dateTime.toLocaleDateString());
-  console.log(dateTime.toLocaleTimeString('ja-JP'));
+  // console.log(dateTime.toLocaleDateString());
+  // console.log(dateTime.toLocaleTimeString('ja-JP'));
 
   const theme1 = createTheme({
     palette: {
@@ -65,6 +79,11 @@ const Content = (): JSX.Element => {
   });
 
   const navigate = useNavigate();
+  if (!testUser) {
+    return (
+      <div></div>
+    )
+  }
 
   return (
     <div style={{
@@ -117,8 +136,8 @@ const Content = (): JSX.Element => {
           }}
         />
         <img
-          src={heart?heart1:heart0}
-          onClick={()=>setHeart(heart?false:true)}
+          src={heart ? heart1 : heart0}
+          onClick={() => setHeart(heart ? false : true)}
           style={{
             width: "3vw",
             height: "3vw",
@@ -137,8 +156,8 @@ const Content = (): JSX.Element => {
             backgroundPosition: "center center",
           }}
         />
-        <text style={{marginLeft:"1vw"}}>
-          {dateTime.toLocaleDateString()+" "+dateTime.toLocaleTimeString('ja-JP')}
+        <text style={{ marginLeft: "1vw" }}>
+          {dateTime ? dateTime.toLocaleDateString() + " " + dateTime.toLocaleTimeString('ja-JP') : ""}
         </text>
       </div>
       <div style={{ gridColumn: "2/4", gridRow: "4/5", margin: "0 auto", paddingTop: "20vh" }}>
@@ -147,9 +166,9 @@ const Content = (): JSX.Element => {
         </Stack>
       </div>
       <div style={{ gridColumn: "3/4", gridRow: "3/5", textAlign: "center" }}>
-        <div style={{ }}>
+        <div style={{}}>
           {/* 幸せ度表示 */}
-          <div style={{ fontSize: "5vh", margin: "3vh", marginBottom: "5vh", marginTop: "7vh", gridColumn: "2/3", gridRow: "3/4", color: "#f39c12",}}>
+          <div style={{ fontSize: "5vh", margin: "3vh", marginBottom: "5vh", marginTop: "7vh", gridColumn: "2/3", gridRow: "3/4", color: "#f39c12", }}>
             {"Happy! " + testUser.happiness_rate + "%"}
           </div>
         </div>
